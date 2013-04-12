0764e554461b0fa5c54da11d331f0957f35c2a4e
Tonny Nerd <tonnydourado@gmail.com>
2013-04-12 09:40:04 -0300

    Added bootstrap, removed 'printer' code, and other minor updates.

--- project/controllers/static.py
+++ project/controllers/static.py
@@ -8,6 +8,6 @@ def favicon(file):
     return static_file(file, root='project/static/misc')
 
 
-@app.route('/:path#(images|css|js|fonts)\/.+#')
+@app.route('/:path#(img|css|js|fonts)\/.+#')
 def server_static(path):
     return static_file(path, root='project/static')
